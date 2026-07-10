e0c2322e0cefedaf01c4fda8d32f6fe32147b109
c2e-money <dipenbiswas9101@gmail.com>
2026-07-10 10:13:25 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,9 @@
+import "./globals.css";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* Poori website ka background #0b0e14 set kar rahe hain */}
-      <body className="bg-[#0b0e14] text-white min-h-screen">
+      <body className="min-h-screen bg-[#0b0e14]">
         {children}
       </body>
     </html>
