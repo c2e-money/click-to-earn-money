1a4d0464b7348dc47a1329fdf0b24f092288f09e
c2e-money <dipenbiswas9101@gmail.com>
2026-07-10 12:06:59 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {},
-  },
+  content: ["./app/**/*.{js,jsx}"],
+  theme: { extend: {} },
   plugins: [],
-}
-
+};
