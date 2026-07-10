2d64e2b7549f0ffef9b9b14d480ca707c41c2abd
c2e-money <dipenbiswas9101@gmail.com>
2026-07-10 17:52:39 +0530

    Create firebase.js

---
+++ app/lib/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD3Yl0BR4o6qEX6MeXYjX6Qjlr5BCid5C8",
+  authDomain: "my-website-242fc.firebaseapp.com",
+  projectId: "my-website-242fc",
+  storageBucket: "my-website-242fc.firebasestorage.app",
+  messagingSenderId: "78108710064",
+  appId: "1:78108710064:web:7b5e79f33721fbc7f71775"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
