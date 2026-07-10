ea2e6517fbb86ba65c2cca1414548330c6b3d038
c2e-money <dipenbiswas9101@gmail.com>
2026-07-10 12:06:20 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,8 @@
 import "./globals.css";
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-[#0b0e14] text-gray-200 antialiased min-h-screen">
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
