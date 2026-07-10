c27ccfdfca2db50031101915d6656f2d93131221
c2e-money <dipenbiswas9101@gmail.com>
2026-07-10 20:23:15 +0530

    Update page.js

--- app/[alias]/page.js
+++ app/[alias]/page.js
@@ -3,20 +3,15 @@ import { doc, getDoc, updateDoc, increment } from "firebase/firestore";
 import { redirect } from "next/navigation";
 
 export default async function RedirectPage({ params }) {
-  const { alias } = params; // Yahan [alias] folder ke naam se match kiya
-
+  const { alias } = params;
   try {
     const urlRef = doc(db, "urls", alias);
     const urlSnap = await getDoc(urlRef);
-
     if (urlSnap.exists()) {
-      const data = urlSnap.data();
       await updateDoc(urlRef, { clicks: increment(1) });
-      redirect(data.originalUrl);
+      redirect(urlSnap.data().originalUrl);
     }
-  } catch (error) {
-    console.error("Redirect Error:", error);
-  }
+  } catch (error) { console.error("Redirect Error:", error); }
 
   return (
     <div className="h-screen flex items-center justify-center bg-[#0b0e14] text-white font-black">
