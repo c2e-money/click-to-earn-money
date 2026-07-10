21ebb5927f45474d3b56910b9a7f7b81223c6e90
c2e-money <dipenbiswas9101@gmail.com>
2026-07-10 12:53:41 +0530

    Create Navbar.js

---
+++ app/components/Navbar.js
@@ -0,0 +1,14 @@
+// app/components/Navbar.js
+import { Home, Link2, Wallet } from "lucide-react";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="fixed bottom-0 w-full bg-[#0b0e14] border-t border-[#1f2937] p-3 flex justify-around z-50">
+      <Link href="/dashboard" className="flex flex-col items-center"><Home size={20} /><span className="text-[9px] font-black uppercase mt-1">Home</span></Link>
+      <Link href="/dashboard/links" className="flex flex-col items-center"><Link2 size={20} /><span className="text-[9px] font-black uppercase mt-1">Links</span></Link>
+      <Link href="/dashboard/withdraw" className="flex flex-col items-center"><Wallet size={20} /><span className="text-[9px] font-black uppercase mt-1">Withdraw</span></Link>
+    </nav>
+  );
+    }
+    
