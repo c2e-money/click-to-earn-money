92dd0df28b60cd49ca4c900dbe8c2e11324c06f0
c2e-money <dipenbiswas9101@gmail.com>
2026-07-15 16:46:02 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,16 @@
-// app/layout.js
 import "./globals.css";
+
+export const metadata = {
+  title: "Click To Earn | URL Shortener",
+  description: "Highest paying URL shortener",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-[#0b0e14] text-white h-screen">{children}</body>
+      <body className="bg-[#0b0e14] text-white h-screen font-sans">
+        {children}
+      </body>
     </html>
   );
 }
