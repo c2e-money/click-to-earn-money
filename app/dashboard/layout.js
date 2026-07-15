2ff708c73b9a36c7c0d8c8c0de9903be922cf375
c2e-money <dipenbiswas9101@gmail.com>
2026-07-15 20:28:33 +0530

    Update layout.js

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,8 +1,3 @@
 export default function DashboardLayout({ children }) {
   return <>{children}</>;
 }
-// Dashboard.js mein useEffect ke andar:
-if (doc.exists() && doc.data().isBanned) {
-    alert("Your account is banned!");
-    signOut(auth);
-}
